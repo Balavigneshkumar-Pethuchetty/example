9a5d34792827d251b6163171bf9e65da6297cca4
Balavigneshkumar-Pethuchetty <33866877+Balavigneshkumar-Pethuchetty@users.noreply.github.com>
2018-11-15 00:33:47 +0530

    initial commit

---
+++ largeApp/src/app/app.component.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactSingleComponent } from './contact-single/contact-single.component';
+import { CreateContactComponent } from './create-contact/create-contact.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        ContactListComponent,
+        ContactSingleComponent,
+        CreateContactComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'largeApp'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('largeApp');
+  });
+
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to largeApp!');
+  });
+});
